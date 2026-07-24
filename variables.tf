8547040c8883f9c5d4c1c25322c602623d61d6a3
AbhishekSharma1102 <abhishekk.ssharma11@gmail.com>
2026-07-24 12:24:10 +0530

    Updated the project with new features

---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "aws_region" {
+  description = "AWS Region where resources will be created"
+  type        = string
+}
+variable "project_name" {
+  description = "Project name used for naming AWS resources"
+  type        = string
+}
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for Public Subnet 1"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for Public Subnet 2"
+  type        = string
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for Private Subnet 1"
+  type        = string
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for Private Subnet 2"
+  type        = string
+}
+variable "availability_zone_1" {
+  description = "Availability Zone for Public and Private Subnet 1"
+  type        = string
+}
+
+variable "availability_zone_2" {
+  description = "Availability Zone for Public and Private Subnet 2"
+  type        = string
+}
+variable "db_name" {
+  description = "Initial PostgreSQL database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Master username for PostgreSQL"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for PostgreSQL"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance type"
+  type        = string
+}
+
+variable "allocated_storage" {
+  description = "Allocated storage for the RDS instance (GB)"
+  type        = number
+}
+
+variable "engine_version" {
+  description = "PostgreSQL engine version"
+  type        = string
+}
